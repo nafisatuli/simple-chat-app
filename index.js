c6972147cd49bc40718fa30adde1785144b45164
nafisatuli <nafisahasan98@gmail.com>
2022-09-22 10:31:00 +0600

    io configure

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const express = require('express');
 const app = express();
 
 const http = require('http');
-const server = http.createServer(app);
+const expressServer = http.createServer(app);
 
+// configure socket
+const { Server } = require('socket.io')
+
+let io = new Server(expressServer);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 
-server.listen(3000, function () {
+io.on('connection', function (socket) {
+    console.log("new user connected")
+})
+
+expressServer.listen(3000, function () {
     console.log("server running at 3000")
 })
